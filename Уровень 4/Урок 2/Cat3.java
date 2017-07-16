0da0750c3d785d28b9ea1722e923764638a4ce26
Greed <kanzew@outlook.com>
2017-07-16 15:46:39 +0500

    4

---
+++ Уровень 4/Урок 2/Cat3.java
@@ -0,0 +1,34 @@
+/**
+*		Реализовать метод setName
+*	Реализовать метод setName, чтобы с его помощью можно было устанавливать значение переменной private String fullName равное значению локальной переменной String fullName.
+*
+*
+*		Требования:
+*	1.Класс Cat должен содержать только одну переменную fullName.
+*	2.Переменная fullName должна иметь модификатор доступа private и тип String.
+*	3.Класс Cat должен содержать только два метода setName и main.
+*	4.Метод setName должен иметь локальную переменную fullName.
+*	5.Метод setName класса Cat должен устанавливать значение глобальной переменной private String fullName равным локальной переменной fullName.
+*/
+
+package com.javarush.task.task04.task0406;
+
+/** 
+*	Реализовать метод setName
+*	@version 0.1 2017.07.16
+*	@author Константин
+*/
+
+public class Cat3 {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+
+        this.fullName = fullName;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

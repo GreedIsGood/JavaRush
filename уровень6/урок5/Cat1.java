6b410c96587543df29ed18b89591980ab96b8506
Greed <kanzew@outlook.com>
2017-08-02 23:04:56 +0500

    14

---
+++ уровень6/урок5/Cat1.java
@@ -0,0 +1,37 @@
+/**
+*		Пустые кошки, пустые псы
+*	В каждом классе Cat и Dog написать метод finalize, который выводит на экран текст о том, что такой-то объект уничтожен.
+*
+*
+*		Требования:
+*	1.В классе Cat должен быть void метод finalize.
+*	2.В классе Dog должен быть void метод finalize.
+*	3.Метод finalize класса Cat должен выводить на экран "Cat was destroyed".
+*	4.Метод finalize класса Dog должен выводить на экран "Dog was destroyed".
+*/
+
+package com.javarush.task.task06.task0602;
+
+/**
+*	Пустые кошки, пустые псы
+*	@version 0.1 2017.08.02
+*	@author Константин
+*/
+
+public class Cat1 {
+    public static void main(String[] args) {
+
+    }
+
+    protected void finalize() throws Throwable{
+        System.out.println("Cat was destroyed");
+    }
+
+}
+
+class Dog {
+    protected void finalize() throws Throwable{
+        System.out.println("Dog was destroyed");
+    }
+    
+}

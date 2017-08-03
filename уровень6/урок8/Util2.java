276507570fba314b3fdd3b6a6155898f82418cef
Greed <kanzew@outlook.com>
2017-08-03 20:46:29 +0500

    15

---
+++ уровень6/урок8/Util2.java
@@ -0,0 +1,31 @@
+/**
+*		Расстояние между двумя точками
+*	Реализовать статический метод double getDistance(x1, y1, x2, y2). Он должен вычислять расстояние между точками.
+*	Используй метод double Math.sqrt(double a), который вычисляет квадратный корень переданного параметра.
+*
+*
+*		Требования:
+*	1.Метод getDistance должен возвращать double.
+*	2.Метод getDistance должен быть статическим.
+*	3.Метод getDistance должен быть public.
+*	4.Метод getDistance должен возвращать расстояние между точками.
+*	5.Метод getDistance должен использовать метод double Math.sqrt(double a).
+*/
+
+package com.javarush.task.task06.task0609;
+
+/**
+*	Расстояние между двумя точками
+*	@version 0.1 2017.08.03
+*	@author Константин
+*/
+
+public class Util2 {
+    public static double getDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(((x2 - x1) * (x2-x1))+((y2-y1) *(y2-y1)));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
